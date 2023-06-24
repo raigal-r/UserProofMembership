989917d40eba37617c7e30f9ce945337e17800ea
Julio M Cruz <julio.cruz@eb-ms.net>
2023-06-24 14:55:02 -0400

    Migrate to Next JS

---
+++ Frontend/src/config/sismo-connect-config.tsx
@@ -0,0 +1,7 @@
+// sismo-connect-config.ts
+
+import { SismoConnectConfig } from "@sismo-core/sismo-connect-react";
+
+export const config: SismoConnectConfig = {
+  appId: "0xef7f9c59d297bdad9b2beb8e47f34bce", // replace with your appId
+};
