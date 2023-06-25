053aceb7ad7ed331331c4a717d4ef788e198e952
Julio M Cruz <julio.cruz@eb-ms.net>
2023-06-25 07:41:03 -0400

    add token wallet app

---
+++ TokenWalletApp/const/constants.ts
@@ -0,0 +1,15 @@
+import { Mumbai, Chain } from '@thirdweb-dev/chains'
+
+// your token bound factory address
+export const factoryAddress: string = '0x02101dfB77FDE026414827Fdc604ddAF224F0921'
+export const implementation: string = '0x86Ce468CA4288847A62E61B64c5e728583972e7F'
+
+// Your thirdweb api key - you can get one at https://thirdweb.com/dashboard/api-keys
+export const TWApiKey: string =
+    '481e3560e864ee555ca136eda0ef06bc36c76d4f7d3597e14dc040a1e5351f932236273498a730f612e90367cd626ab1773bab9997fcc821db13adda61790ac3'
+export const activeChain: Chain = Mumbai
+
+export const nftDropAddress: string = '0x961B4382744EB6690280358EFcb0C0fCb0BC8abA'
+export const tokenAddress: string = '0x971B6CFa32f54008A326Bfc330ce9D6B307C7199'
+
+export const EDITIONDROP_ADDRES:string= '0xd570C41B6c7356D878Da9FDA173fEb7A5854EAC1'
